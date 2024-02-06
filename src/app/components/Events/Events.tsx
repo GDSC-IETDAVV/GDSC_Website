53481473c007cda4dbc271a2c35ae9804a8ad287
Mustafa_Azad <21bcs038@ietdavv.edu.in>
2024-02-06 23:40:38 +0530

    changes in website

--- src/app/components/Events/Events.tsx
+++ src/app/components/Events/Events.tsx
@@ -5,7 +5,7 @@ const Events = () => {
   const events = [
     {
       imageUrl:
-        "https://instagram.fbho1-2.fna.fbcdn.net/v/t51.2885-15/338914928_605667961141642_6142937004483739776_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyIn0&_nc_ht=instagram.fbho1-2.fna.fbcdn.net&_nc_cat=110&_nc_ohc=eNFQhdBIFSAAX8VdXRf&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzA3MjE4NzMzNDk5MjcwNjA0Mg%3D%3D.2-ccb7-5&oh=00_AfAi6dQw0V3t040w7YQwG9vCLJODDmF9VHexD8lkPOyZ_Q&oe=65BFCD15&_nc_sid=b41fef",
+        "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707237226/Cod-A-Thon_id4mpp.jpg",
       eventName: "COD-A-THON",
       eventDescription:
         "GDSC IET DAVV'S very own competitive programming competition which happened last year made students learn, grow and realize that 'Winning isn't everything, it's only thing'",
@@ -17,7 +17,7 @@ const Events = () => {
     },
     {
       imageUrl:
-        "https://instagram.fbho1-4.fna.fbcdn.net/v/t51.2885-15/367437504_256265503939349_2033191662829496470_n.heic?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyIn0&_nc_ht=instagram.fbho1-4.fna.fbcdn.net&_nc_cat=105&_nc_ohc=iiQ6p15FDuoAX9d-a2j&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzE2OTM0MTY5NTM3NTU4MDgwMw%3D%3D.2-ccb7-5&oh=00_AfD1cdsjaGL3Q_VViIdv1xY3n1Y-Uy3u4fwITwas2c6xWQ&oe=65C0C6AB&_nc_sid=b41fef",
+        "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707237227/TechUnleash_jakmx4.jpg",
       eventName: "TechUnleash",
       eventDescription:
         "With the programming running in our veins and codes beating in our hearts, We the team GDSC IET DAVV encouraged students to join our club and gain knowledge about programming through our idea 'TechUnleash'.",
@@ -28,8 +28,7 @@ const Events = () => {
       registrationUrl: "http://bit.ly/gdsctechunleash",
     },
     {
-      imageUrl:
-        "https://instagram.fbho1-2.fna.fbcdn.net/v/t51.2885-15/371418579_1322772091960811_4185986814030058177_n.heic?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyIn0&_nc_ht=instagram.fbho1-2.fna.fbcdn.net&_nc_cat=111&_nc_ohc=mRwfzXMo2dEAX9O1Ske&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzE4MDI2OTQ4NDkwMzUxNTI3MQ%3D%3D.2-ccb7-5&oh=00_AfAoMn2pbfgXUVNSIWlaksHxTIc297cI3JDqdbm9pVoygg&oe=65C22BA7&_nc_sid=b41fef",
+      imageUrl: "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707237226/Cloud_mcuhe8.jpg",
       eventName: "Google Cloud Facilitator Program 2023",
       eventDescription:
         "We provided a program to help students boost their career in cloud computing and AI. GDSC IET DAVV also provided access to full-fledged courses provided by Google, with many surprises and goodies!",
@@ -40,8 +39,7 @@ const Events = () => {
       registrationUrl: "https://forms.gle/Bjtora7GyiKFtZCY7",
     },
     {
-      imageUrl:
-        "https://instagram.fbho1-1.fna.fbcdn.net/v/t51.2885-15/379386778_332383305924607_31396196133864739_n.heic?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyIn0&_nc_ht=instagram.fbho1-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=bvPBEbtBt04AX_svEUG&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MzE5NDc0MDEyODQwMzc1ODIzMw%3D%3D.2-ccb7-5&oh=00_AfAzO0yQEqNdN8JqGVGKw38q8y9iKQ_uYs3HeDMoAuMZww&oe=65C01A0E&_nc_sid=b41fef",
+      imageUrl: "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707237226/Code_Quest_qdiyfy.jpg",
       eventName: "CodeQuest 2023",
       eventDescription:
         "To let our students flex those coding muscles, we brought a coding contest. And guess what? It turned out to be an epic saga of coding events which gave us memories to remember and learning to never forget!",
@@ -52,7 +50,7 @@ const Events = () => {
       registrationUrl: "https://forms.gle/73iTZW591cAuyYd69",
     },
     {
-      imageUrl: "URL_TO_IMAGE_5",
+      imageUrl: "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707237227/Invento_x18oah.jpg",
       eventName: "GDSC Volunteer meet-up",
       eventDescription:
         "We believe that if there's a name it's because of the family. We couldn't be more grateful to our volunteers for always being there with us. And We showed them our respect and thankfulness by organizing a meet up with them. We had fun, made memories, and promised to stay and grow with GDSC IET DAVV.",
@@ -63,8 +61,7 @@ const Events = () => {
       registrationUrl: "https://forms.gle/N9zbd5oR2MAKr9YJ8",
     },
     {
-      imageUrl:
-        "https://instagram.fbho1-2.fna.fbcdn.net/v/t51.2885-15/386387457_1443136846249221_8005438878321973709_n.webp?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyIn0&_nc_ht=instagram.fbho1-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=x_Rxc5XEyhEAX-DmX-y&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzIwNjkyODk0MzA2MzIxMjUzMA%3D%3D.2-ccb7-5&oh=00_AfB8i8JM01YuqKGBVTm4GzuNTN_dXGf081f1W27UHnVWDg&oe=65C22A8C&_nc_sid=ee9879",
+      imageUrl: "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707237226/Hacktober_Fest_sszqj6.jpg",
       eventName: "Hacktoberfest",
       eventDescription:
         "In collaboration with GDSC IIPS and GDSC SDSF We GDSC IET DAVV successfully organized an event which sent for learners to contribute to open source projects and enhance their coding skills!",
@@ -84,8 +81,7 @@ const Events = () => {
       registrationUrl: "https://gdsc.community.dev/j/skgky5g9nta7w/",
     },
     {
-      imageUrl:
-        "https://instagram.fbho1-1.fna.fbcdn.net/v/t51.2885-15/400191592_7067596153262570_3167590708174003889_n.heic?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyIn0&_nc_ht=instagram.fbho1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=KMA4ULIIM10AX8XukcQ&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MzIzMTYxNTk4MDQ0MTU3OTYzNg%3D%3D.2-ccb7-5&oh=00_AfCBVjD5BB8RArwV-T25R2dVQjr0bTPBG2cad-DrTz_-JQ&oe=65C2675A&_nc_sid=ee9879",
+      imageUrl: "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707237226/Code_Series_vl5xap.jpg",
       eventName: "CODE SERIES",
       eventDescription:
         "30 Day's coding session which made all of us say code like a pro, compile like a dope. Our students believed in their own selves and stood their grounds to achieve this challenge given by us. It was definitely a Proud moment for us to see our students believe in their coding ability!!",
--- src/app/components/Teams/Teams.tsx
+++ src/app/components/Teams/Teams.tsx
@@ -106,8 +106,7 @@ const leads = [
   {
     name: "Anuj Malviya",
     role: "AI & ML Lead",
-    imageUrl:
-      "https://media.licdn.com/dms/image/D4D03AQFBGUeKq2D_vg/profile-displayphoto-shrink_800_800/0/1681651732217?e=1712188800&v=beta&t=Yv-KYfsiCRmpKvHd26J-3-WiZ_AHIeEIpwfkwr4qJRM",
+    imageUrl: "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707158248/anujimg_mibf25.jpg",
     bio: "Hi it's Anuj, I'm also MLSA of IET DAVV, navigating the exciting realms of technology. My passion lies in crafting innovative solutions through Python Automations, Machine Learning .In my spare time I like to practice Oragami, play chess,I’m always down for hearing about new projects, so feel free to drop me a line.",
     githubUrl: "https://github.com/Anuj-malviya0",
     linkedinUrl: "https://www.linkedin.com/in/anuj-malviya-a531b4204/",
@@ -123,8 +122,7 @@ const leads = [
   {
     name: "Atharva Sugandhi",
     role: "DSA & CP Lead",
-    imageUrl:
-      "https://media.licdn.com/dms/image/D4D03AQFBGUeKq2D_vg/profile-displayphoto-shrink_800_800/0/1681651732217?e=1712188800&v=beta&t=Yv-KYfsiCRmpKvHd26J-3-WiZ_AHIeEIpwfkwr4qJRM",
+    imageUrl: "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707242591/athvimg_z5qwfu.jpg",
     bio: "I'm Atharva Sugandhi, a Software Developer specializing in mastering Data Structures and Algorithms. My passion lies in tackling complex coding puzzles with efficiency. Alongside, I excel in crafting engaging web experiences using React.",
     githubUrl: "https://github.com/Atom-Atharva",
     linkedinUrl: "https://www.linkedin.com/in/atharva-sugandhi-391a4b225/",
@@ -133,7 +131,7 @@ const leads = [
     name: "Tushar Gupta",
     role: " Management Lead",
     imageUrl:
-      "https://drive.google.com/file/d/1MlU7g9qXA-noGvZD4eDpSHiXlkC_Jfa4/view?usp=drivesdk",
+      "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707240736/tushrimg_skdm7s.jpg",
     bio: "I am a MERN developer and and ML enthusiast also have keen interest in management field ",
     xUrl: "https://twitter.com/TusharrrGupTA",
     linkedinUrl: "https://www.linkedin.com/in/tushar-gupta-39048a224",
@@ -142,7 +140,7 @@ const leads = [
     name: "Dev Sahani",
     role: "DSA & CP Lead",
     imageUrl:
-      "https://drive.google.com/file/d/1T7DWXmtwANn-yWuyg-95h3h76kg1ATXF/view?usp=drivesdk",
+      "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707158239/devimg_lwcpc7.jpg",
     bio: "As the competitive programming lead at GDSC IET DAVV, I'm skilled in DSA and competitive programming, and I absolutely enjoy tackling problems. With MERN development skills and Next.js knowledge, I offer versatile expertise.",
     linkedinUrl: "https://www.linkedin.com/in/dev-sahani/",
     githubUrl: "https://github.com/Dev-Sahani",
@@ -151,7 +149,7 @@ const leads = [
     name: "Daksh Sahani",
     role: "DSA & CP Lead",
     imageUrl:
-      "https://drive.google.com/file/d/1SjQ3p5X6GgI3ldBb74FRAkAdY8Ch-uA9/view?usp=drivesdk",
+      "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707158239/dakshimg_apnfxu.jpg",
     bio: "I am Daksh Sahani, DSA & CP Lead at GDSC. I have a passion for problem-solving and CP, utilizing DSA to tackle coding challenges. . Also, I'm a MERN developer, with hands-on experience in socket io and WebRTC Technology. Let's connect for anything related to programming or beyond!",
     linkedinUrl: "https://www.linkedin.com/in/daksh-sahani-2b6ab71b2/",
     githubUrl: "https://github.com/DakshSahani",
@@ -159,8 +157,7 @@ const leads = [
   {
     name: "Insiya Husain",
     role: "Flutter and Content Lead",
-    imageUrl:
-      "https://media.licdn.com/dms/image/D4D03AQGlhoYawUnj6A/profile-displayphoto-shrink_800_800/0/1697568770960?e=1712188800&v=beta&t=4Fzq-WG2-pvhA1odzovYvlVISMY8LxqBs9Nv51OLzIQ",
+    imageUrl: "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707158237/insiyaimg_jeeguc.jpg",
     bio: "I love connecting the dots whether it's blending ideas from different fields, bringing people from diverse teams together, or combining applications from various industries. I enjoy being a Flutter Developer, plus I'm always exploring the exciting world of AI for new insights. I also enjoy sharing my thoughts through blogs, diving into the latest trends in robotics and AI. Beyond my technical skills, I firmly believe in the transformative power of self-development.",
     githubUrl: "https://github.com/Insiyaya",
     linkedinUrl: "https://www.linkedin.com/in/insiya-husain-89b501259?",
@@ -168,8 +165,7 @@ const leads = [
   {
     name: "Arunima Agrawal",
     role: "Management Lead and Social Media Lead",
-    imageUrl:
-      "https://media.licdn.com/dms/image/D4D03AQEToBJ4qX_AYQ/profile-displayphoto-shrink_800_800/0/1706719705628?e=1712188800&v=beta&t=dA13G24XB0eJ-RN2474gLkSxsqr8oYGXstxJGW0SoAA",
+    imageUrl: "https://res.cloudinary.com/dhwbs4kk2/image/upload/v1707240940/arunimaimg_pdn4cu.png",
     bio: "My journey is defined by a commitment to excellence, innovation, and knowledge in the sphere of tech. I'm a web developer who loves creating cool things online. Currently, I'm exploring data science to learn some new tricks. I'm also good at managing projects to keep everything running smoothly.",
     githubUrl: "https://github.com/ArunimaAgrawal",
     linkedinUrl: "https://www.linkedin.com/in/arunima-agrawal-881479226",
@@ -180,25 +176,6 @@ export default function Teams() {
   return (
     <div className="bg-white py-16 md:py-24 lg:py-32 pt-40">
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-20 px-6 lg:px-8">
-        {/* <div className="mx-auto max-w-2xl lg:mx-0">
-					<div className="fixed w-[36rem]">
-					<h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-						Meet Our Team
-					</h2>
-					<p className="mt-6 text-lg leading-8 text-gray-600 text-justify pr-32">
-						Welcome to the heart of our GDSC community, Our team is a diverse
-						group of passionate individuals, each bringing unique skills and
-						perspectives to the table. From coding wizards to creative thinkers,
-						our members are united by a shared enthusiasm for technology and a
-						commitment to fostering a vibrant learning environment. Get to know
-						the faces behind the code – the leaders, mentors, and enthusiasts
-						who make GDSC a hub for growth, knowledge-sharing, and friendship.
-						Together, we&apos;re on a mission to explore, innovate, and empower
-						the next generation of tech leaders. Meet the minds shaping the
-						future of our community!
-					</p>
-					</div>
-				</div> */}
         <div className="space-y-10">
           <TeamCards people={mentors} title="Mentors" />
           <TeamCards people={leads} title="Leads" />
