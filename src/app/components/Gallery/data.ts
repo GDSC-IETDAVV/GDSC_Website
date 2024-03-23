bd3f2a7269781d2fd09db2c95154e8fcf1e23c0c
kanishk6103 <mamta11tiwari06@gmail.com>
2024-03-23 22:44:48 +0530

    another try

--- src/app/components/Gallery/data.ts
+++ src/app/components/Gallery/data.ts
@@ -3,7 +3,7 @@ import Moi2 from "../../../../public/moi2.jpg";
 import techUnleash from "../../../../public/techUnleash.jpg";
 import techUnleash2 from "../../../../public/techUnleash2.jpg";
 import felicitation from "../../../../public/felicitation.jpg";
-import vaibhavSir from "../../../../public/vaibhavSir.jpg";
+import vaibhavSir from "../../../../public/vaibhavSir.png";
 import flutter from "../../../../public/flutter.jpg";
 import uiux from "../../../../public/uiux.jpg";
 
