3f605a212fc948597c89eb3fee6e9982de9c4501
kanishk6103 <mamta11tiwari06@gmail.com>
2024-03-23 22:42:09 +0530

    another error, another fix

--- src/app/components/Gallery/data.ts
+++ src/app/components/Gallery/data.ts
@@ -2,10 +2,10 @@ import Moi from "../../../../public/moi.jpg";
 import Moi2 from "../../../../public/moi2.jpg";
 import techUnleash from "../../../../public/techUnleash.jpg";
 import techUnleash2 from "../../../../public/techUnleash2.jpg";
-import felicitation from "../../../../public/felicitation.jpg"
-import vaibhavSir from "../../../../public/vaibhavSir.jpg"
-import flutter from "../../../../public/flutter.jpg"
-import uiux from "../../../../public/uiux.jpg"
+import felicitation from "../../../../public/felicitation.jpg";
+import vaibhavSir from "../../../../public/vaibhavSir.jpg";
+import flutter from "../../../../public/flutter.jpg";
+import uiux from "../../../../public/uiux.jpg";
 
 export {
     Moi,
