7c1097c901157f71a256a114a5d1478213095d66
kanishk6103 <mamta11tiwari06@gmail.com>
2024-03-23 23:58:34 +0530

    added analytics

--- src/app/Context.tsx
+++ src/app/Context.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { AppStateProvider } from "../utils/appStateContext";
 import { Poppins } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -13,7 +13,10 @@ interface contextProps {
 const Context = ({ props }: contextProps) => {
   return (
     <AppStateProvider>
-      <body className={poppins.className}>{props}</body>
+      <body className={poppins.className}>
+        {props}
+        <Analytics />
+      </body>
     </AppStateProvider>
   );
 };
